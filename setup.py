c4564d2b6ec6845f899dc595dda33a611b8f892d
Ayato <manhattancafe0810520@gmail.com>
2023-10-08 18:31:22 -0500

    changing structure

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="NovelAILLMWrapper",
-    version="0.1",
+    version="0.102",
     packages=find_packages(),
     install_requires=[
 
